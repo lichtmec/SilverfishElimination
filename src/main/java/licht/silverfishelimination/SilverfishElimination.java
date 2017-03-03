4b2dc4c34c9d650f0693933492e3b215ffc5b632
licht <ulichit@gmail.com>
2017-03-04 01:51:24 +0900

    エントリクラス作成

---
+++ src/main/java/licht/silverfishelimination/SilverfishElimination.java
@@ -0,0 +1,28 @@
+package licht.silverfishelimination;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="silverfishelimination", name="SilverfishElimination")
+public class SilverfishElimination
+{
+	@Mod.Instance("silverfishelimination")
+	public static SilverfishElimination instance;
+
+	@Mod.EventHandler
+	public void preInit (FMLPreInitializationEvent event)
+	{
+	}
+
+	@Mod.EventHandler
+	public void init (FMLInitializationEvent event)
+	{
+	}
+
+	@Mod.EventHandler
+	public void postInit (FMLPostInitializationEvent event)
+	{
+	}
+}
